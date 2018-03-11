6797e7726c1d890ddad530d3e9d3c2e0d55fd323
flackovic <filackovic@gmail.com>
2018-03-11 14:07:42 +0100

    First commit

---
+++ mymodule.php
@@ -0,0 +1,72 @@
+<?php
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class MyModule extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'mymodule';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0';
+        $this->author = 'Filip Pilifi';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('My module');
+        $this->description = $this->l('Description of my module.');
+
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+        if (!Configuration::get('MYMODULE_NAME')) {
+            $this->warning = $this->l('No name provided');
+        }
+    }
+
+    public function install()
+    {
+        if (Shop::isFeatureActive()) {
+            Shop::setContext(Shop::CONTEXT_ALL);
+        }
+
+        if (!parent::install() ||
+            !$this->registerHook('actionOrderStatusPostUpdate') ||
+            !Configuration::updateValue('MYMODULE_NAME', 'my friend')
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function uninstall()
+    {
+        if (!parent::uninstall() ||
+            !Configuration::deleteByName('MYMODULE_NAME')
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function hookActionOrderStatusPostUpdate($params)
+    {
+
+        /**
+         * Check if order is considered paid.
+         * This should, later on, check order status explicitly by name/id since some are
+         * considered paid but are not candidates for fiscal
+         *
+         * TODO: Implement explicit check by id/name
+         */
+        if(!$params['newOrderStatus']->paid) {
+            return;
+        }
+
+    }
+}
